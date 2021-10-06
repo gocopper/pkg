64cee2fd62bf3c5d325e35fa2fa8c4d457858aa0
Tushar Soni <tusharsoni1205@gmail.com>
2021-10-05 21:45:38 -0400

    Add ctx and error returns to cpubsub

--- cpubsub/local.go
+++ cpubsub/local.go
@@ -1,21 +1,29 @@
 package cpubsub
 
+import (
+	"context"
+
+	"github.com/gocopper/copper/clogger"
+)
+
 // LocalPubSub is an implementation of PubSub that only allows publishing
 // of payloads to 'local' subscribers i.e. subscribers on the same instance
 // as the publisher.
 type LocalPubSub struct {
 	subscriptions map[string][]Handler
+	logger        clogger.Logger
 }
 
 // NewLocalPubSub creates a new LocalPubSub
-func NewLocalPubSub() PubSub {
+func NewLocalPubSub(logger clogger.Logger) PubSub {
 	return &LocalPubSub{
 		subscriptions: make(map[string][]Handler),
+		logger:        logger,
 	}
 }
 
 // Subscribe register the handler to the topic. When a payload is published on the topic, the handler will be called.
-func (l *LocalPubSub) Subscribe(topic string, handler Handler) error {
+func (l *LocalPubSub) Subscribe(ctx context.Context, topic string, handler Handler) error {
 	handlers, ok := l.subscriptions[topic]
 	if !ok {
 		handlers = make([]Handler, 0)
@@ -28,14 +36,21 @@ func (l *LocalPubSub) Subscribe(topic string, handler Handler) error {
 
 // Publish publishes the payload on the given topic. All handlers subscribed to the topic will be called with the
 // payload.
-func (l *LocalPubSub) Publish(topic string, payload interface{}) error {
+func (l *LocalPubSub) Publish(ctx context.Context, topic string, payload interface{}) error {
 	handlers, ok := l.subscriptions[topic]
 	if !ok {
 		return nil
 	}
 
-	for _, h := range handlers {
-		go h(payload)
+	for i := range handlers {
+		go func(i int) {
+			err := handlers[i](ctx, payload)
+			if err != nil {
+				l.logger.WithTags(map[string]interface{}{
+					"topic": topic,
+				}).Error("Failed to run pubsub handler", err)
+			}
+		}(i)
 	}
 
 	return nil
--- cpubsub/pubsub.go
+++ cpubsub/pubsub.go
@@ -1,10 +1,12 @@
 package cpubsub
 
+import "context"
+
 // Handler defines a function that is called when a payload is published on the subscribed topic
-type Handler func(payload interface{})
+type Handler func(ctx context.Context, payload interface{}) error
 
 // PubSub defines the methods for publishing payloads to a topic and subscribing to topics with a handler
 type PubSub interface {
-	Subscribe(topic string, handler Handler) error
-	Publish(topic string, payload interface{}) error
+	Subscribe(ctx context.Context, topic string, handler Handler) error
+	Publish(ctx context.Context, topic string, payload interface{}) error
 }
